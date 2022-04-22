ebfd0f68d228daeffdce25fe1218d01bfb22def8
Blastradius <>
2022-04-22 11:56:30 +0200

    Fix occasional crash on invalid sense entities

--- src/lib/features/Sense.ts
+++ src/lib/features/Sense.ts
@@ -13,7 +13,7 @@ export class Sense {
 
   private collectChanges(localPlayer: app.Player, players: Array<app.Player>, pointers: Array<app.Pointer>, mode?: string) {
     for (const x of players) {
-      if (x.isLocal) continue;
+      if (x.isLocal || [0, 255].includes(x.glowEnable.value)) continue;
       const dx = (localPlayer.localOrigin.value.x - x.localOrigin.value.x) * 0.0254;
       const dy = (localPlayer.localOrigin.value.y - x.localOrigin.value.y) * 0.0254;
       const r = Math.sqrt(Math.pow(dx, 2) + Math.pow(dy, 2));
