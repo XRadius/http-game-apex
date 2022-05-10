be7403a3d43ef89b4c9f44ad951e2f931f704dc8
OffsetsBot <>
2022-05-10 18:10:52 +0000

    Automatic Offsets

--- src/lib/core/offsets/coreOffsets.ts
+++ src/lib/core/offsets/coreOffsets.ts
@@ -1,5 +1,5 @@
 export const coreOffsets = {
-  levelName: BigInt('0x1350fa0'),
-  clEntityList: BigInt('0x19ed718'),
-  localPlayer: BigInt('0x1d9e418')
+  levelName: BigInt('0x135cd80'),
+  clEntityList: BigInt('0x19fbc18'),
+  localPlayer: BigInt('0x1dac9c8')
 };
