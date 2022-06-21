485f03238214f5dde481fa7944adacc6f1329f23
OffsetsBot <>
2022-06-21 18:09:00 +0000

    Automatic Offsets

--- src/lib/core/offsets/coreOffsets.ts
+++ src/lib/core/offsets/coreOffsets.ts
@@ -1,5 +1,5 @@
 export const coreOffsets = {
-  levelName: BigInt('0x135ce40'),
-  clEntityList: BigInt('0x19fbf98'),
-  localPlayer: BigInt('0x1dacd48')
+  levelName: BigInt('0x137e370'),
+  clEntityList: BigInt('0x1a1e3b8'),
+  localPlayer: BigInt('0x1dcf5e8')
 };
--- src/lib/core/offsets/playerOffsets.ts
+++ src/lib/core/offsets/playerOffsets.ts
@@ -1,5 +1,5 @@
 export const playerOffsets = {
   lifeState: BigInt('0x798'),
-  viewAngles: BigInt('0x2588'),
-  bleedoutState: BigInt('0x2728')
+  viewAngles: BigInt('0x2580'),
+  bleedoutState: BigInt('0x2720')
 };
