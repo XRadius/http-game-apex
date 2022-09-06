11a63a5e2e453ed734330ba0c2f5e35d377691ec
arturzxc <arturzxc@hotmail.com>
2022-09-06 20:36:13 +0100

    Recoil - fixed the pull down of crosshairs when tapping a weapon

--- src/lib/features/Recoil.ts
+++ src/lib/features/Recoil.ts
@@ -3,15 +3,11 @@ import * as app from '.';
 export class Recoil {
   private vecPunchWeaponAngle = app.core.VectorData.none;
 
-  update(buttonList: app.core.ButtonList, localPlayer: app.core.Player, options: IRecoilOptions) {
-    if (localPlayer.viewAngle.source.syncId) {
-      /* We sent an update, but did not receive confirmation! */
-    } else if (!buttonList.inAttack.value) {
-      this.vecPunchWeaponAngle = app.core.VectorData.none;
-    } else {
+  update(localPlayer: app.core.Player, options: IRecoilOptions) {
+    if (!localPlayer.viewAngle.source.syncId) {
       const vecPunchWeaponAngle = localPlayer.vecPunchWeaponAngle.value;
       const viewAngle = localPlayer.viewAngle.value;
-      if (Math.abs(vecPunchWeaponAngle.x) >= 0.5 || Math.abs(vecPunchWeaponAngle.y) >= 0.5 || Math.abs(vecPunchWeaponAngle.z) >= 0.5) {
+      if (Math.abs(vecPunchWeaponAngle.x) > 0 || Math.abs(vecPunchWeaponAngle.y) > 0) {
         const x = viewAngle.x + (this.vecPunchWeaponAngle.x - vecPunchWeaponAngle.x) * options.percentage;
         const y = viewAngle.y + (this.vecPunchWeaponAngle.y - vecPunchWeaponAngle.y) * options.percentage;
         localPlayer.viewAngle.delta(new app.core.VectorData(x, y, viewAngle.z));
