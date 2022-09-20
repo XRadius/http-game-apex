cef927f44681ec7293de7333317358efb44f841e
Giorgio D'Alessandro <giorgio.dalessandro@dxc.com>
2022-09-20 22:25:58 +0200

    update: offsets @ 20/09/2022

--- src/lib/core/offsets.ts
+++ src/lib/core/offsets.ts
@@ -1,12 +1,12 @@
 export const offsets = {
   button: {
-    inSpeed: 0x759bfa0,               // [Buttons]                        -> in_speed
-    inAttack: 0x759bff8               // [Buttons]                        -> in_attack
+    inSpeed: 0x75c6208,               // [Buttons]                        -> in_speed
+    inAttack: 0x75c6260               // [Buttons]                        -> in_attack
   },
   core: {
-    levelName: BigInt(0x13a17b8),     // [Miscellaneous]                  -> LevelName
-    clEntityList: BigInt(0x1a75038),  // [Miscellaneous]                  -> cl_entitylist
-    localPlayer: BigInt(0x1e25418)    // [Miscellaneous]                  -> LocalPlayer
+    levelName: BigInt(0x13c3e38),     // [Miscellaneous]                  -> LevelName
+    clEntityList: BigInt(0x1a98778),  // [Miscellaneous]                  -> cl_entitylist
+    localPlayer: BigInt(0x1e49288)    // [Miscellaneous]                  -> LocalPlayer
   },
   entity: {
     localOrigin: 0x158,               // [DataMap.CBaseViewModel]         -> m_localOrigin
@@ -20,11 +20,11 @@ export const offsets = {
   player: {
     glowEnable: 0x3c8,                // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x8
     glowThroughWall: 0x3d0,           // [RecvTable.DT_HighlightSettings] -> m_highlightServerContextID + 0x10
-    iTeamNum: 0x448,                  // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
+    iTeamNum: 0x44c,                  // [RecvTable.DT_BaseEntity]        -> m_iTeamNum
     iName: 0x589,                     // [RecvTable.DT_BaseEntity]        -> m_iName
     lifeState: 0x798,                 // [RecvTable.DT_Player]            -> m_lifeState
-    vecPunchWeaponAngle: 0x23f8,      // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
-    viewAngle: 0x24e0,                // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
-    bleedoutState: 0x2688             // [RecvTable.DT_Player]            -> m_bleedoutState
+    vecPunchWeaponAngle: 0x2440,      // [DataMap.C_Player]               -> m_currentFrameLocalPlayer.m_vecPunchWeapon_Angle
+    viewAngle: 0x253c,                // [DataMap.C_Player]               -> m_ammoPoolCapacity - 0x14
+    bleedoutState: 0x26d0             // [RecvTable.DT_Player]            -> m_bleedoutState
   }
 };
